428c53b752e92fa90e87878bf610354f98be8fda
murodbey <abdullayevmurod@gmail.com>
2018-11-03 15:26:52 +0000

    changed for jenkins

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "instance2" {
   ami                         = "${lookup(var.amis, var.region)}"
   instance_type               = "t2.micro"
-  key_name                    = "project1"
+  key_name                    = "jenkins"
   subnet_id                   = "${aws_subnet.artemis-public.id}"
   vpc_security_group_ids      = ["${aws_security_group.artemis.id}"]
   user_data                   = "${file("apache.sh")}"
@@ -20,7 +20,7 @@ resource "aws_instance" "instance2" {
 resource "aws_instance" "dbhost" {
   ami                    = "${lookup(var.amis, var.region)}"
   instance_type          = "t2.micro"
-  key_name               = "project1"
+  key_name               = "jenkins"
   subnet_id              = "${aws_subnet.artemis-public.id}"
   vpc_security_group_ids = ["${aws_security_group.artemis.id}"]
   user_data              = "${file("db.sh")}"
